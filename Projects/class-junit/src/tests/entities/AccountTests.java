085355cd2f237272eaafc444e66f97404e19d6bb
MatheusDario <dario.matheus97@gmail.com>
2022-05-24 11:04:52 -0300

    completd class tests

--- Projects/class-junit/src/tests/entities/AccountTests.java
+++ Projects/class-junit/src/tests/entities/AccountTests.java
@@ -7,28 +7,60 @@ import entities.Account;
 import tests.factory.AccountFactory;
 
 public class AccountTests {
-	
+
 	@Test
 	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
-		
+
 		double amount = 200.0;
 		double expectedValue = 196.0;
 		Account acc = AccountFactory.creatEmptyAccount();
-		
+
 		acc.deposit(amount);
-		
-		Assertions.assertEquals(expectedValue, acc.getBalance());
-	}
-	
-	@Test
-	public void depositShouldDoNothingWhenNegativeAmount() {
-		double expectedValue = 100.;
-		Account acc = AccountFactory.creatAccount(expectedValue);
-		double amount = -200.0;
-		
-		acc.deposit(amount);
-		
+
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
 
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		double expectedValue = 100.0;
+		Account acc = AccountFactory.creatAccount(expectedValue);
+		double amount = -200.0;
+
+		acc.deposit(amount);
+
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalanc() {
+
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.creatAccount(initialBalance);
+
+		double result = acc.fullWithdraw();
+
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+	}
+
+	@Test
+	public void withdrawShouldDecreaseBalanceWhenSufficentBalance() {
+
+		Account acc = AccountFactory.creatAccount(800.0);
+
+		acc.withdraw(500.0);
+
+		Assertions.assertEquals(300.0, acc.getBalance());
+	}
+
+	@Test
+	public void withdrawShoudThrowExceptionWhenInsufficientBalance() {
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Account acc = AccountFactory.creatAccount(800.0);
+
+			acc.withdraw(801.0);
+		} );
+	}
 }
